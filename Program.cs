c8577a232b3d1237f2a42296eebff35de673c640
ET24DE17 <PC-1@PC-GAB1-07>
2023-11-13 12:26:14 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaSoapUI_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SoapUI.CountryInfoService client = new SoapUI.CountryInfoService();
+
+            var respuesta = client.FullCountryInfoAllCountries();
+
+            Console.WriteLine(respuesta.First().sName);
+            Console.WriteLine(respuesta.First().sContinentCode);
+            Console.WriteLine(respuesta.First().sCurrencyISOCode);
+            Console.Read();
+        }
+    }
+}
